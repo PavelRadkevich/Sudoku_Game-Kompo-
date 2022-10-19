4c4f17b7686aa63516b715eb8bab6cddd325536c
240365 <240365@edu.p.lodz.pl>
2022-10-19 21:54:44 +0200

    Stworzone testy dla SudokuBoard, przerobione wejściowe parametry dla metody fillBoard

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -3,17 +3,26 @@ import java.util.Collections;
 
 public class SudokuBoard {
     int size = 9;
-    int [][] sudokuBoard = new int [size][size];
-    int [] exrow = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int column = 0, row = 0;
+    int [][] sudokuBoard = new int [row][column];
+    int [] exrow = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+
+
+    public SudokuBoard () {
+
+    }
 
     public int getCell (int x, int y) {
         return sudokuBoard[x][y];
     }
 
-    public void fillBoard(int [][] sudokuBoard, int row, int column, int[] exrow) {
-    if (row == size && column == size)
+    public void fillBoard(int row, int column) {
+    if (row == size-1 && column == size) {
+        column = 0;
+        row = 0;
         return;
+    }
 
     if (column == size) {
         row++;
@@ -24,12 +33,20 @@ public class SudokuBoard {
     for (int i = 1; i <= 9; i++) {
         if (checkCell(sudokuBoard, i, row, column)) {
             sudokuBoard[row][column] = i;
-            fillBoard(sudokuBoard, row, column++, exrow);
+            fillBoard(row, column++);
         }
     }
 
     }
 
+    public void printBoard () {
+        for (int i=0; i<9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudokuBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     boolean checkCell (int [][]sudokuBoard, int num, int x, int y) {
         for (int i = 0; i < 9; i++){
@@ -46,6 +63,8 @@ public class SudokuBoard {
                     return false;
             }
         }
+
+
         return true;
     }
 
