e8d72400d55101c8dea70a2adbc77b202fc8b6fa
242422 Kisiel Mikołaj <242422@edu.p.lodz.pl>
2022-10-24 19:24:17 +0200

    Every is right?

--- First_project/src/test/java/org/example/SudokuBoardTest.java
+++ First_project/src/test/java/org/example/SudokuBoardTest.java
@@ -10,89 +10,6 @@ public class SudokuBoardTest {
 
     public SudokuBoardTest() {
     }
-    /*@Test
-    public void testFillBoard() {
-        SudokuBoard board1 = new SudokuBoard();
-        SudokuBoard board2 = new SudokuBoard();
-        board1.fillBoard();
-        board2.fillBoard();
-        int match = 0;
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    if (board1.getCell(i, j) ==board2.getCell(i, j)) {
-                        match++;
-                    }
-                }
-            }
-        assertNotEquals(81, match);
-    }
-
-   /* @Test
-    public void testNotRepeat() {
-        SudokuBoard board = new SudokuBoard();
-        board.fillBoard();
-        int match = 0;
-            for (int x = 0; x < 9; x++) {
-                for (int y = 0; y < 9; y++) {
-                    for (int i = 0; i < 9; i++) {
-                        if (board.getCell(i, y) == board.getCell(x,y)) {
-                            match++;
-                            if (i == x) {
-                                match--;
-                            }
-                        }
-                    }
-                    for (int i = 0; i < 9; i++) {
-                        if (board.getCell(x, i) == board.getCell(x,y)) {
-                            match++;
-                            if (i == y) {
-                                match--;
-                            }
-                        }
-                    }
-                    for (int i = 0; i <= 2; i++) {
-                        for (int j = 0; j <= 2; j++) {
-                            if (board.getCell(i + x - x % 3, j + y - y % 3) == board.getCell(x,y)) {
-                                match++;
-                                if (i + x - x % 3 == x && j + y - y % 3 != y) {
-                                    match--;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        assertEquals(0, match);
-    }
-    @Test
-    public void testSumOfColumns() {
-        SudokuBoard board = new SudokuBoard();
-        board.fillBoard();
-        int sum = 0;
-
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sum = sum + board.getCell(i, j);
-                }
-                assertEquals(45, sum);
-                sum = 0;
-            }
-    }
-
-    @Test
-    public void testSumOfRows() {
-        SudokuBoard board = new SudokuBoard();
-        board.fillBoard();
-        int sum = 0;
-
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sum = sum + board.getCell(j, i);
-                }
-                assertEquals(45, sum);
-                sum = 0;
-            }
-    }*/
 
     @Test
     public void testFillBoard() {
@@ -150,7 +67,7 @@ public class SudokuBoardTest {
         assertEquals(0, match);
     }
 
-    @Test
+    /*@Test
     public void testSumOfColumns() {
         SudokuBoard board = new SudokuBoard();
         board.fillBoard();
@@ -192,7 +109,7 @@ public class SudokuBoardTest {
         catch(Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
     @Test
     public void testRepeatInBox() {
@@ -206,8 +123,8 @@ public class SudokuBoardTest {
                 for (int y = 0; y < 9; y++) {
                     for (int i = 0; i <= 2; i++) {
                         for (int j = 0; j <= 2; j++) {
-                            if (m.invoke(i + x - x % 3, j + y - y % 3) == m.invoke(x, y)) {
-                                if((i + x - x % 3) != x && (j + y - y % 3) != y) {
+                            if (m.invoke(board,i + x - x % 3, j + y - y % 3) == m.invoke(board,x, y)) {
+                                if ((i + x - x % 3) != x && (j + y - y % 3) != y) {
                                     match++;
                                 }
                             }
@@ -220,5 +137,27 @@ public class SudokuBoardTest {
         catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void testCorrectRange() {
+        SudokuBoard board = new SudokuBoard();
+        board.fillBoard();
+        int wrong = 0;
+        try {
+            Method m = SudokuBoard.class.getDeclaredMethod("getCell", int.class, int.class);
+            m.setAccessible(true);
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if ((int)m.invoke(board,i,j) < 1 || (int)m.invoke(board,i,j) > 9) {
+                        wrong++;
+                    }
+                }
+            }
+            assertEquals(0, wrong);
+        }
+        catch(Exception e) {
+        e.printStackTrace();
+    }
     }
 }
