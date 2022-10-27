51ec328169b69dd347965a7c1b9ea0cced401700
242422 Kisiel Mikołaj <242422@edu.p.lodz.pl>
2022-10-27 10:06:13 +0000

    Main.java edited online with Bitbucket

--- First_project/src/main/java/org/example/Main.java
+++ First_project/src/main/java/org/example/Main.java
@@ -3,8 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
-        SudokuBoard b = new SudokuBoard();
-        b.solveGame();
-        b.printBoard();
     }
 }
