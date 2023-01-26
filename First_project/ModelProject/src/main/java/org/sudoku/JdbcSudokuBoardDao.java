45d5fc4fb8682ba8e41db3403792c934a45b8b4a
240365 <240365@edu.p.lodz.pl>
2023-01-26 12:56:52 +0100

    12 Problem z połączeniem do bazy

--- First_project/ModelProject/src/main/java/org/sudoku/JdbcSudokuBoardDao.java
+++ First_project/ModelProject/src/main/java/org/sudoku/JdbcSudokuBoardDao.java
@@ -12,8 +12,11 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
     }
     @Override
     public void write (SudokuBoard sudokuBoard) {
+
+        //////////SudokuBoard///////////////////
         String url = "jdbc:sqlite:./" + filename;
         Connection connection;
+        ResultSet key;
         try {
             connection = DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -21,21 +24,57 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
         }
         String sqlCreate = """
                 CREATE TABLE IF NOT EXISTS SudokuBoard (
-                	id integer PRIMARY KEY,
+                	ID INT IDENTITY(1,1) NOT NULL PRIMARY,
                 	name text NOT NULL,
                 );""";
         String sqlInsert = "INSERT INTO SudokuBoard(name) VALUES(?)";
         try (Statement stmt = connection.createStatement();) {
             stmt.execute(sqlCreate);
+            key = stmt.getGeneratedKeys();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
 
         try (PreparedStatement pstmt = connection.prepareStatement(sqlInsert)) {
             pstmt.setString(1, filename);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+
+
+        /////////////SudokuField/////////////////
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                String sqlFieldCreate = """
+                    CREATE TABLE IF NOT EXISTS SudokuField (
+                	ID INT IDENTITY(1,1) NOT NULL PRIMARY,
+                	value INT NOT NULL,
+                	row INT NOT NULL,
+                	column INT NOT NULL,
+                	box INT NOT NULL,
+                	board INT NOT NULL,
+                	FOREIGN KEY (board) REFERENCES SudokuBoard(ID)
+                );""";
+            String sqlFieldInsert = "INSERT INTO SudokuField(value, row, column, box, board) VALUES(?)";
+            try (Statement stmt = connection.createStatement();) {
+                stmt.execute(sqlFieldCreate);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            try (PreparedStatement pstmt = connection.prepareStatement(sqlFieldInsert)) {
+                pstmt.setInt(1, sudokuBoard.getCell(i, j));
+                pstmt.setInt(2, sudokuBoard.getRow(j).numberOfGroup);
+                pstmt.setInt(3, sudokuBoard.getColumn(i).numberOfGroup);
+                pstmt.setInt(4, sudokuBoard.getBox(i, j).numberOfGroup);
+                //pstmt.setInt(5, key);
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            }
         }
     }
 
@@ -50,5 +89,6 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
         }
         return null;
     }
+
 
 }
--- First_project/ModelProject/src/main/java/org/sudoku/Main.java
+++ First_project/ModelProject/src/main/java/org/sudoku/Main.java
@@ -1,6 +1,13 @@
 package org.sudoku;
 
+import org.sudoku.exception.IndexOutRange;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IndexOutRange {
+        BackTrackingSudokuSolver bs = new BackTrackingSudokuSolver();
+        SudokuBoard sb = new SudokuBoard(bs);
+        bs.solve(sb);
+        JdbcSudokuBoardDao jdbc = new JdbcSudokuBoardDao("d");
+        jdbc.write(sb);
     }
 }
